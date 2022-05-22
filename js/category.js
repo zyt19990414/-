3915c6318e409acedd047d6ee47c7e5677259553
张已涛 <1151259301@qq.com>
2022-05-22 16:51:38 +0800

    '二次提酷玩页面布局完成'

---
+++ js/category.js
@@ -0,0 +1,6 @@
+// 头部部分
+
+var header_1 = document.getElementsByClassName('header_1');
+header_1[1].style.color = '#FE5341';
+
+
---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+// 点击加载更多部分
+var e3 = document.getElementsByClassName('e3')[0];
+// console.log(e3);
+var e3_2 = document.getElementsByClassName('e3_2')[0]
+// console.log(e3_2);
+e3.onclick = function () {
+    e3.style.display = 'none';
+    e3_2.style.display = 'block'
+}
+
+// 头部部分
+
+var header_1 = document.getElementsByClassName('header_1');
+header_1[0].style.color = '#FE5341';
